6987672ba207a72399654110e695804622857e6f
jungekatz <vibhorsenvee@gmail.com>
2020-10-19 22:55:48 +0530

    [main] added componet for polar area

--- twitter-word-cloud/src/app/dashboard-module/dashboard-module.module.ts
+++ twitter-word-cloud/src/app/dashboard-module/dashboard-module.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { DashboardModuleRoutingModule } from './dashboard-module-routing.module';
 import { SidebarComponent } from './sidebar-component/sidebar-component.component';
 import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
+import { PolarAreaComponent } from './polar-area/polar-area.component';
 
 @NgModule({
-  declarations: [SidebarComponent, MainDashboardComponent],
+  declarations: [SidebarComponent, MainDashboardComponent, PolarAreaComponent],
   imports: [CommonModule, DashboardModuleRoutingModule],
 })
 export class DashboardModule {}
