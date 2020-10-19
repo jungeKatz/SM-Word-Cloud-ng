0a0062e794e2ee7a0153856c9e0d9e969e09576d
jungekatz <vibhorsenvee@gmail.com>
2020-10-19 22:52:54 +0530

    [main] Added main dhashboard component

--- twitter-word-cloud/src/app/dashboard-module/dashboard-module.module.ts
+++ twitter-word-cloud/src/app/dashboard-module/dashboard-module.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardModuleRoutingModule } from './dashboard-module-routing.module';
 import { SidebarComponent } from './sidebar-component/sidebar-component.component';
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
 
 @NgModule({
-  declarations: [SidebarComponent],
+  declarations: [SidebarComponent, MainDashboardComponent],
   imports: [CommonModule, DashboardModuleRoutingModule],
 })
 export class DashboardModule {}
