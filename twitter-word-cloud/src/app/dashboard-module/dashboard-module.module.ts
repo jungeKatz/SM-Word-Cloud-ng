e382a7660d5aaed8b8103a23694ffd119e196512
jungekatz <vibhorsenvee@gmail.com>
2020-10-19 22:56:36 +0530

    [main] Added component for word cloud

--- twitter-word-cloud/src/app/dashboard-module/dashboard-module.module.ts
+++ twitter-word-cloud/src/app/dashboard-module/dashboard-module.module.ts
@@ -5,9 +5,10 @@ import { DashboardModuleRoutingModule } from './dashboard-module-routing.module'
 import { SidebarComponent } from './sidebar-component/sidebar-component.component';
 import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
 import { PolarAreaComponent } from './polar-area/polar-area.component';
+import { WordCloudComponent } from './word-cloud/word-cloud.component';
 
 @NgModule({
-  declarations: [SidebarComponent, MainDashboardComponent, PolarAreaComponent],
+  declarations: [SidebarComponent, MainDashboardComponent, PolarAreaComponent, WordCloudComponent],
   imports: [CommonModule, DashboardModuleRoutingModule],
 })
 export class DashboardModule {}
