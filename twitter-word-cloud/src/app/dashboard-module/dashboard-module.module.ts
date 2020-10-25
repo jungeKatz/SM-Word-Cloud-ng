6dd2f68db92db8ae8fcc0ab6766abdfb33a6a72b
jungeKatz <jungekatz@outlook.com>
2020-10-26 00:34:54 +0530

    [wordCloud-feature-independent] Added reactive form module

--- twitter-word-cloud/src/app/dashboard-module/dashboard-module.module.ts
+++ twitter-word-cloud/src/app/dashboard-module/dashboard-module.module.ts
@@ -7,9 +7,10 @@ import { MainDashboardComponent } from './main-dashboard/main-dashboard.componen
 import { PolarAreaComponent } from './polar-area/polar-area.component';
 import { WordCloudComponent } from './word-cloud/word-cloud.component';
 import { SearchUtilComponent } from './search-util/search-util.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [SidebarComponent, MainDashboardComponent, PolarAreaComponent, WordCloudComponent, SearchUtilComponent],
-  imports: [CommonModule, DashboardModuleRoutingModule],
+  imports: [CommonModule, DashboardModuleRoutingModule, FormsModule, ReactiveFormsModule],
 })
 export class DashboardModule {}
