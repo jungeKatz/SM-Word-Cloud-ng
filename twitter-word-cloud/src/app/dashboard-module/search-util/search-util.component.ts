dd6781472e1fb31d4ef681e4e0a78e18fe338c0b
jungeKatz <jungekatz@outlook.com>
2020-10-26 00:35:44 +0530

    [wordCloud-feature-independent] Added form element for search

--- twitter-word-cloud/src/app/dashboard-module/search-util/search-util.component.ts
+++ twitter-word-cloud/src/app/dashboard-module/search-util/search-util.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search-util',
   templateUrl: './search-util.component.html',
-  styleUrls: ['./search-util.component.scss']
+  styleUrls: ['./search-util.component.scss'],
 })
 export class SearchUtilComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  constructor() {}
 
   ngOnInit(): void {
+    this.createSearchForm();
   }
+  createSearchForm(): void {
+    this.form = new FormGroup({
+      noOfResults: new FormControl(''),
+      needWord: new FormControl(''),
+      needPie: new FormControl(''),
+    });
+  }
+
+onSubmit(): void{
+console.log('onSubmit');
+console.log(this.form.value);
+
+}
+
 
 }
