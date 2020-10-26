8141f30022c9887c50a917237041f9cd7e3279d3
jungeKatz <jungekatz@outlook.com>
2020-10-26 07:30:23 +0530

    [wordCloud-feature-independent] editing , added todo

--- twitter-word-cloud/src/app/dashboard-module/dashboard-module.module.ts
+++ twitter-word-cloud/src/app/dashboard-module/dashboard-module.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+/*
+ *Import Components After this
+ */
 import { DashboardModuleRoutingModule } from './dashboard-module-routing.module';
 import { SidebarComponent } from './sidebar-component/sidebar-component.component';
 import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
 import { PolarAreaComponent } from './polar-area/polar-area.component';
 import { WordCloudComponent } from './word-cloud/word-cloud.component';
 import { SearchUtilComponent } from './main-dashboard/search-util/search-util.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [SidebarComponent, MainDashboardComponent, PolarAreaComponent, WordCloudComponent, SearchUtilComponent],
-  imports: [CommonModule, DashboardModuleRoutingModule, FormsModule, ReactiveFormsModule],
+  declarations: [
+    SidebarComponent,
+    MainDashboardComponent,
+    PolarAreaComponent,
+    WordCloudComponent,
+    SearchUtilComponent,
+  ],
+  imports: [
+    CommonModule,
+    DashboardModuleRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class DashboardModule {}
