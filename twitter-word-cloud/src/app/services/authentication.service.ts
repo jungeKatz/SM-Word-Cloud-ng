7cc2e7ee0b4b12448b948c09f520d1308b854765
jungekatz <jungekatz@outlook.com>
2020-11-01 22:48:51 +0530

    [twitter-Oauth] Added Auth service

--- twitter-word-cloud/src/app/services/authentication.service.ts
+++ twitter-word-cloud/src/app/services/authentication.service.ts
@@ -2,14 +2,44 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
+
 import { map } from 'rxjs/operators';
-import { environment } from '../../environments/environment';
-// import { User } from './../models';
+import { environment } from '@environments/environment';
+import { User } from '@app/models/users';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthenticationService {
+  private userSubject: BehaviorSubject<User>;
+  public user: Observable<User>;
 
-  constructor() { }
+  constructor(private router: Router, private http: HttpClient) {
+    this.userSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('user')));
+    this.user = this.userSubject.asObservable();
+  }
+
+
+    // tslint:disable-next-line: typedef
+    login(username: string, password: string) {
+        return this.http.post<any>(`${environment.apiUrl}/users/authenticate`, { username, password })
+            .pipe(map(user => {
+                // store user details and basic auth credentials in local storage to keep user logged in between page refreshes
+                user.authdata = window.btoa(username + ':' + password);
+                localStorage.setItem('user', JSON.stringify(user));
+                this.userSubject.next(user);
+                return user;
+            }));
+    }
+
+
+    // tslint:disable-next-line: typedef
+    logout() {
+      // remove user from local storage to log user out
+      localStorage.removeItem('user');
+      this.userSubject.next(null);
+      this.router.navigate(['']);
+  }
+
+
 }
