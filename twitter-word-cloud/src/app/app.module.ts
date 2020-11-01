8981c8c0ea6eba5b2146593e4e34a828e3e8d97a
jungekatz <jungekatz@outlook.com>
2020-11-01 19:22:24 +0530

    [twitter-Oauth] added intercepting backend

--- twitter-word-cloud/src/app/app.module.ts
+++ twitter-word-cloud/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { TagCloudModule } from 'angular-tag-cloud-module';
+import {HttpClientModule} from '@angular/common/http';
+// import *  as omri  from 'omri-beautify';
+
 
 import { AppComponent } from './app.component';
 import { LoginComponentComponent } from './login-component/login-component.component';
@@ -17,6 +20,7 @@ import { DashboardModule } from './dashboard-module/dashboard-module.module';
     ReactiveFormsModule,
     DashboardModule,
     TagCloudModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
